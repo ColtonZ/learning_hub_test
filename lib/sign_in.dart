5f4a80b8a260590c46d99fd740bd2b8d78006a3c
Zack Colton <coltonz@stpaulsschool.org.uk>
2020-04-23 14:17:44 +0100

    Added testing sign-in code

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -5,32 +5,8 @@ final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
 Future<String> signInWithGoogle() async {
-  print("yes");
-  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-  final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
-
-  final AuthCredential credential = GoogleAuthProvider.getCredential(
-    accessToken: googleSignInAuthentication.accessToken,
-    idToken: googleSignInAuthentication.idToken,
-  );
-
-  final AuthResult authResult = await _auth.signInWithCredential(credential);
-  final FirebaseUser user = authResult.user;
-
-  assert(!user.isAnonymous);
-  assert(await user.getIdToken() != null);
-
-  final FirebaseUser currentUser = await _auth.currentUser();
-  assert(user.uid == currentUser.uid);
-
-  print('signInWithGoogle succeeded: $user');
-}
-
-void signOutGoogle() async {
-  await googleSignIn.signOut();
-
-  print("User Signed Out");
+  GoogleSignIn _googleSignIn = GoogleSignIn();
+  await _googleSignIn.signIn();
 }
 
 //https://medium.com/flutter-community/flutter-sign-in-with-google-in-android-without-firebase-a91b977d166f
