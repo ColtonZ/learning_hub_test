bdf531a191574c02108528d33f818e9f09429338
Zack Colton <coltonz@stpaulsschool.org.uk>
2020-04-28 20:30:33 +0100

    Got API and sign in working together

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Future<String> getData() async {
-    http.Response response = await http.get(
-        Uri.encodeFull("https://classroom.googleapis.com/v1/courses"),
-        headers: {
-          "key": "AIzaSyBZ5izrPKmflMeDuGiJbZ0eudN1FD3OU1o",
-          "Accept": "application/json"
-        });
-
-    print(response.body);
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -38,7 +27,7 @@ class MyApp extends StatelessWidget {
                 child: Text('Get Data'),
               ),
               MaterialButton(
-                onPressed: signInWithGoogle,
+                onPressed: null,
                 color: Colors.red,
                 textColor: Colors.black,
                 child: Text('Sign In'),
